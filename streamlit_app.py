ed1461b4a60069b71bb11d1796f35da312bf2fdc
Yuna-Liu-VCC <yliu378@volvocars.com>
2025-06-03 00:20:46 +0200

    new markdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,24 +7,19 @@ st.set_page_config(
 
 st.write("# Meet Yuna Liu – A Structured Mind for an Unstructured World! 👋")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Select an aspect above.")
 
-st.markdown(
-    """
-    Experienced Data Engineer & Data Scientist | Azure, Snowflake, Databricks Expert
+st.markdown("""
+This dashboard consists of several pages, each highlighting an important aspect of my professional journey:
 
-Hi, I’m Yuna Liu, a results-driven data engineer and data scientist with a PhD in Economics and extensive experience in leveraging cutting-edge technologies to build robust data pipelines, enable insightful analytics, and deliver impactful business solutions.
+- 💼 **Work Experience** — Explore my career milestones and roles.  
+- 🎓 **Education** — Details about my academic background.  
+- 🎖️ **Certifications** — Professional certificates I earned.  
+- ❄️ **Chatbot Demo** — Interactive demo showcasing AI chatbot capabilities I developed developed as part of a GenAI project.  
+- 📚 **Publications** — A timeline view of my research publications.
 
-Currently, I work as a consultant at Capgemini, where I contribute to Volvo Cars' Sales Dashboard team. My expertise includes working across the entire data pipeline, from extracting and transforming data with Matillion and Snowflake to creating visually compelling and data-intensive dashboards in Power BI.
+Use the sidebar to navigate between pages. Enjoy exploring! 😊
+""")
 
-Over the years, I’ve had the privilege of leading diverse projects, such as:
-
-Developing scalable pipelines for cell durability data at Volvo Cars, improving performance.
-Building ML models to predict default probabilities at Norion Bank using Databricks and Python.
-Creating analytical solutions to empower decision-makers with accurate, actionable data at Volvo Cars.
-
-I hold certifications in Azure Data Engineering (DP-203), Snowflake, and Databricks (Databricks Machine Learning Associate, Databricks Generative AI Engineer Associate) etc. My technical skills span SQL, Python, Power BI, Delta Lake, Feature Store, and AI/ML tools, and I am passionate about learning and applying emerging technologies.
-
-I thrive on tackling complex challenges, enabling data-driven decision-making, and contributing to innovative solutions. Let’s connect if you’re interested in data engineering, machine learning, or innovative business intelligence solutions! 🚀
-"""
-)
+st.markdown("---")
+st.caption("Created with ❤️ by Yuna Liu")
